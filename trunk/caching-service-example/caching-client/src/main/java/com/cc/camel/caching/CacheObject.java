d2616f5f3e1959f872285b8ac12de50c7d50c273
dean.m.harrington@gmail.com <dean.m.harrington@gmail.com@091a29f0-e6af-11bd-8522-1576d184d21e>
2013-01-10 16:14:55 +0000

    Initial import.

---
+++ trunk/caching-service-example/caching-client/src/main/java/com/cc/camel/caching/CacheObject.java
@@ -0,0 +1,16 @@
+package com.cc.camel.caching;
+
+import java.io.Serializable;
+
+/**
+ * Cacheable class for use with the CachingClient.
+ *
+ * @author Dean Harrington
+ * @version 0.0.1-SNAPSHOT
+ *
+ */
+public class CacheObject implements Serializable {
+
+    /** Generated Serial VersionID.*/
+    private static final long serialVersionUID = 2271357411329686484L;
+}
